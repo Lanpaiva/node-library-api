beeab3308cc37e2d3db550b4ad0c63858e4ef92e
Lanpaiva <alanpaiva@mail.io>
2023-08-05 09:02:26 -0300

    chore: nodemon added to devDependencies

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http')
 const PORT = 3000;
 
+
+const routes = {
+    '/': 'Node library',
+    '/books': 'Books page',
+    '/authors': 'authors page',
+    '/publish': 'publish page',
+    '/about': 'about page'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('node')
+    res.end(routes[req.url])
 })
 
 server.listen(PORT, () => {
-    console.log(`listening on link http://localhost:${PORT}`)
+    console.log(`listening on server http://localhost:${PORT}`)
 })
