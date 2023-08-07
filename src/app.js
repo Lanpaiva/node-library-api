1e7c10dfa803b461b615d71a6bec5c3b990cb977
Lanpaiva <alanpaiva@mail.io>
2023-08-07 15:58:40 -0300

    refactor: functions crud moves to routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import books from "./models/Book.js";
 import routes from "./routes/index.js";
 
 db.on('error', console.log.bind(console, 'connection error!!!'))
@@ -12,34 +11,5 @@ const app = express();
 app.use(express.json())
 routes(app)
 
-
-app.get('/books/:id', (req, res) => {
-    let index = findBook(req.params.id);
-    res.json(books[index])
-})
-
-
-app.post('/books', (req, res) => {
-    books.push(req.body)
-    res.status(201).send("Created")
-})
-
-app.put('/books/:id', (req, res) => {
-    let index = findBook(req.params.id);
-    books[index].title = req.body.title;
-    res.json(books)
-})
-
-app.delete('/books/:id', (req, res) => {
-    let {id} = req.params;
-    let index = findBook(id);
-    books.splice(index, 1);
-    res.send(`book at position id: ${id} deleted from library`)
-})
-
-
-const findBook = (id) => {
-    return books.findIndex(book => book.id == id)
-}
 
 export default app;
