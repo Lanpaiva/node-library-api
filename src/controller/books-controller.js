471497047d509d79e41ae573e7d908974e3e050b
Lanpaiva <alanpaiva@mail.io>
2023-08-07 15:56:54 -0300

    feat: crud database setted

--- src/controller/books-controller.js
+++ src/controller/books-controller.js
@@ -7,6 +7,18 @@ class BookController {
         })
     }
 
+    static listById = (req, res) => {
+        const id = req.params.id
+
+        books.findById(id, (err, books) => {
+            if (err) {
+                res.status(400).send({message: `${err.message} - id not found`})
+            } else {
+                res.status(200).send(books)
+            }
+        })
+    }
+
     static createBooks = (req, res) => {
         let book = new books(req.body)
 
@@ -18,6 +30,30 @@ class BookController {
             }
        })
     }
+
+    static updateBook = (req, res) => {
+
+        const id = req.params.id
+        books.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({message: 'book updated!'})
+            } else {
+               res.status(500).send({message: `${err.message} - fail to update... :(`})
+            }
+        })
+    }
+
+   static removeBook = (req, res) => {
+    const id = req.params.id;
+
+    books.findByIdAndDelete(id, (err) => {
+        if (err) {
+            res.status(500).send({message: err.message})
+        } else {
+            res.status(200).send({message: `book at position id: ${id} deleted from library`})
+        }
+    })
+   }
 }
 
 export default BookController;
